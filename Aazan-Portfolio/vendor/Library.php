6545fe0b650342ed6f718b563aa7cc65a4f050af
Aazan Khan Pathan <aazank517@gmail.com>
2024-05-27 12:49:54 +0500

    anchor changes

--- Aazan-Portfolio/vendor/Library.php
+++ Aazan-Portfolio/vendor/Library.php
@@ -76,7 +76,7 @@
                     </div>
                     <div class="sm:hidden hidden" id="mobile-menu">
                         <div class="space-y-1 px-2 pb-3 pt-2">
-                            <a href="#" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Dashboard</a>
+                            <a href="#" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Home</a>
                         </div>
                     </div>
                 </nav>
